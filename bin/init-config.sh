acf07a33e0d5651a4e086f9c10daf622b3756b15
Weidong <zhaoweidong@gouuse.cn>
2021-07-11 15:48:40 +0800

    update

--- bin/init-config.sh
+++ bin/init-config.sh
@@ -3,13 +3,12 @@ echo 'start'
 # OVPN_OTP_AUTH=1 Google Auth.
  docker-compose run --rm \
      -e 'OVPN_PORT=2298' \
-     -e 'OVPN_DEFROUTE=0' \
      openvpn-server ovpn_genconfig \
      -u 'udp://OVPN.EXAMPLE.COM:2298' \
      -n '223.5.5.5' \
      -n '119.29.29.29' \
-     -r '192.168.1.0/24' \
-     -s '10.8.8.0/24' \
+     -s '10.8.2.0/24' \
+     -p 'route 192.168.255.0 255.255.255.0' \
      -k '10 120' \
      -C 'AES-256-CBC' \
      -e 'reneg-sec 18000' \
@@ -20,10 +19,9 @@ echo 'start'
      -e 'max-clients 50' \
      -e 'client-connect scripts/connect.sh' \
      -e 'client-disconnect scripts/disconnect.sh' \
+     -E 'persist-key' \
+     -E 'persist-tun' \
      -E 'max-routes 10000' \
      -E 'auth-user-pass' \
      -E 'auth-nocache' \
-     -b \
-     -d \
-     -z 
-     
+     -z
